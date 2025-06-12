57a56d127918a5b9ed167f2740d37b8008e9a594
artem <qtqt.art@gmail.com>
2025-06-12 20:32:03 +0300

    add 0219

---
+++ src/problems/0219/s1.ts
@@ -0,0 +1,16 @@
+// set + sliding window
+
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const set = new Set<number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+
+    set.add(nums[i]);
+
+    if (set.size > k) {
+      set.delete(nums[i - k]);
+    }
+  }
+  return false;
+}
