5c658d69b9621088434de8c230936e049fc6b567
artem <qtqt.art@gmail.com>
2025-06-14 00:26:47 +0300

    add 0021

---
+++ src/problems/0021/s1.ts
@@ -0,0 +1,30 @@
+import { ListNode } from "@/utils/list-node";
+
+// listnode
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode();
+    dummy.next = list1;
+
+    let curr = dummy;
+    let p1 = list1;
+    let p2 = list2;
+    while (p1 && p2) {
+        if (p1.val < p2.val) {
+            curr.next = p1;
+            p1 = p1.next;
+        } else {
+            curr.next = p2;
+            p2 = p2.next;
+        }
+        curr = curr.next;
+    }
+
+    if (p1) {
+        curr.next = p1;
+    }
+    if (p2) {
+        curr.next = p2;
+    }
+    return dummy.next;
+};
