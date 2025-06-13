0e70c49400ed9a21de8ded25ba448fd72ba32473
artem <qtqt.art@gmail.com>
2025-06-13 16:26:07 +0300

    add 0083

---
+++ src/problems/0083/s1.ts
@@ -0,0 +1,15 @@
+import type { ListNode } from "@/utils/list-node";
+
+// listnode
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+}
