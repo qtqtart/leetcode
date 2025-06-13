b4768b70c974cebce48e205f3412b420cc3aaf82
artem <qtqt.art@gmail.com>
2025-06-14 00:36:54 +0300

    add 0141

---
+++ src/problems/0141/s1.ts
@@ -0,0 +1,16 @@
+import type { ListNode } from "@/utils/list-node";
+
+function hasCycle(head: ListNode | null): boolean {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+};
