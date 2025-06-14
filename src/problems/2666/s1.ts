40bbec8177f345c6b4b6784453310baa34828b06
artem <qtqt.art@gmail.com>
2025-06-15 01:05:28 +0300

    add 2666

---
+++ src/problems/2666/s1.ts
@@ -0,0 +1,23 @@
+// closure
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let isCalled = false;
+
+  return function (...args) {
+    if (!isCalled) {
+      isCalled = true;
+      return fn(...args);
+    } else {
+      return undefined;
+    }
+  };
+}
