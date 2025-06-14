2813683f083bd874e77132d8bcd61cf103b07b84
artem <qtqt.art@gmail.com>
2025-06-15 00:53:57 +0300

    add 2626

---
+++ src/problems/2626/s1.ts
@@ -0,0 +1,12 @@
+// array
+
+type Fn = (accum: number, curr: number) => number;
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  let ans = init;
+
+  for (let i = 0; i < nums.length; i++) {
+    ans = fn(ans, nums[i]);
+  }
+  return ans;
+}
