6d88f0f3edcfa2c0e0930b2ebdb264388d20c936
artem <qtqt.art@gmail.com>
2025-06-14 23:56:26 +0300

    add 2390

---
+++ src/problems/2390/s1.ts
@@ -0,0 +1,12 @@
+function removeStars(str: string): string {
+  const stack: string[] = [];
+
+  for (const char of str) {
+    if (char !== "*") {
+      stack.push(char);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.join("");
+}
