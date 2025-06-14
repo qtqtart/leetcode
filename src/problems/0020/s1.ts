bcba59401397e9d679aae9bd8e3ab781b5723215
artem <qtqt.art@gmail.com>
2025-06-14 23:44:09 +0300

    add 20

---
+++ src/problems/0020/s1.ts
@@ -0,0 +1,22 @@
+function isValid(str: string): boolean {
+  const map = new Map<string, string>([
+    ["(", ")"],
+    ["{", "}"],
+    ["[", "]"],
+  ]);
+  const stack: string[] = [];
+
+  for (const char of str) {
+    if (map.has(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (char !== map.get(stack.pop())) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
