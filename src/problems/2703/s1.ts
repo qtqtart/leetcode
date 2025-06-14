60aea8b8963152c10a2e8dcb8e89624b2d6e88e4
artem <qtqt.art@gmail.com>
2025-06-15 01:01:33 +0300

    add 2703

---
+++ src/problems/2703/s1.ts
@@ -0,0 +1,13 @@
+// array
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length;
+}
