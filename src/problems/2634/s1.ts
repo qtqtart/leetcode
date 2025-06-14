16eaa5ca0d3d8cd83681429da308b278af593484
artem <qtqt.art@gmail.com>
2025-06-15 00:50:40 +0300

    add 2634

---
+++ src/problems/2634/s1.ts
@@ -0,0 +1,14 @@
+// array
+
+type Fn = (n: number, i: number) => any;
+
+function filter(arr: number[], fn: Fn): number[] {
+  const ans: number[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      ans.push(arr[i]);
+    }
+  }
+  return ans;
+}
