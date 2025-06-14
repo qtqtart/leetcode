60bd109391ec26b6c0ef146efe0d6881da4d2e1b
artem <qtqt.art@gmail.com>
2025-06-14 21:52:09 +0300

    add 707

--- src/problems/0707/s1.ts
+++ src/problems/0707/s1.ts
@@ -1,7 +1,17 @@
-import { ListNode } from "@/utils/list-node";
+class Node {
+  public key: number;
+  public val: number;
+  public next: Node | null;
 
-class MyLinkedList {
-  public head: ListNode | null;
+  constructor(key?: number, val?: number, next?: Node | null) {
+    this.key = key !== undefined ? key : 0;
+    this.val = val !== undefined ? val : 0;
+    this.next = next !== undefined ? next : null;
+  }
+}
+
+class LinkedList {
+  public head: Node | null;
   public size: number;
 
   constructor() {
@@ -9,74 +19,83 @@ class MyLinkedList {
     this.size = 0;
   }
 
-  get(index: number): number {
-    if (index < 0 || index >= this.size) {
-      return -1;
-    }
-
+  get(key: number): number {
     let curr = this.head;
-    for (let i = 0; i < index; i++) {
+    while (curr) {
+      if (curr.key === key) {
+        return curr.val;
+      }
       curr = curr.next;
     }
-    return curr.val;
+    return -1;
   }
 
-  addAtHead(val: number): void {
-    const node = new ListNode(val);
-    node.next = this.head;
+  put(key: number, val: number): void {
+    let curr = this.head;
+    while (curr) {
+      if (curr.key === key) {
+        curr.val = val;
+        return;
+      }
+      curr = curr.next;
+    }
 
+    const node = new Node(key, val, this.head);
     this.head = node;
     this.size++;
   }
 
-  addAtTail(val: number): void {
-    const node = new ListNode(val);
+  remove(key: number): void {
     if (!this.head) {
-      this.head = node;
-    } else {
-      let curr = this.head;
-      while (curr.next) {
-        curr = curr.next;
-      }
-      curr.next = node;
-    }
-    this.size++;
-  }
-
-  addAtIndex(index: number, val: number): void {
-    if (index < 0 || index > this.size) {
       return;
     }
 
-    const node = new ListNode(val);
-    if (index === 0) {
-      node.next = this.head;
-      this.head = node;
-    } else {
-      let curr = this.head;
-      for (let i = 0; i < index - 1; i++) {
-        curr = curr!.next;
-      }
-      node.next = curr!.next;
-      curr!.next = node;
-    }
-    this.size++;
-  }
-
-  deleteAtIndex(index: number): void {
-    if (index < 0 || index >= this.size) {
-      return;
-    }
-
-    if (index === 0) {
+    if (this.head.key === key) {
       this.head = this.head.next;
-    } else {
-      let curr = this.head;
-      for (let i = 0; i < index - 1; i++) {
-        curr = curr.next;
-      }
-      curr.next = curr.next.next;
+      this.size--;
+      return;
     }
-    this.size--;
+
+    let prev = this.head;
+    let curr = this.head.next;
+    while (curr) {
+      if (curr.key === key) {
+        prev.next = curr.next;
+        this.size--;
+        return;
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+}
+
+class MyHashMap {
+  private n: number;
+  private buckets: LinkedList[];
+
+  constructor() {
+    this.n = 13;
+    this.buckets = [];
+
+    for (let i = 0; i < this.n; i++) {
+      this.buckets[i] = new LinkedList();
+    }
+  }
+
+  put(key: number, val: number): void {
+    this.buckets[this.hash(key)].put(key, val);
+  }
+
+  get(key: number): number {
+    return this.buckets[this.hash(key)].get(key);
+  }
+
+  remove(key: number): void {
+    this.buckets[this.hash(key)].remove(key);
+  }
+
+  private hash(key: number): number {
+    return key % this.n;
   }
 }
