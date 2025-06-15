51e3399446b7aac30dcc8b06b2b5af9d48f96988
artem <qtqt.art@gmail.com>
2025-06-15 23:43:50 +0300

    add 104

---
+++ src/problems/0104/s2.ts
@@ -0,0 +1,23 @@
+import type { TreeNode } from "@/utils/tree-node";
+
+function maxDepth(root: TreeNode | null): number {
+  function dfs(node: TreeNode | null): number {
+    const stack = [{ node: root, depth: 1 }];
+    let count = 0;
+
+    while (stack.length) {
+      const { node, depth } = stack.pop();
+
+      if (!node) {
+        continue;
+      }
+
+      count = Math.max(count, depth);
+      stack.push({ node: node.left, depth: depth + 1 });
+      stack.push({ node: node.right, depth: depth + 1 });
+    }
+    return count;
+  }
+
+  return dfs(root);
+}
